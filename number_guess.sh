55b829c74c2c595e3c9c3e521d004b2ea9c6a334
Shantanu Vedak <vedaksv@protonmail.com>
2024-07-23 16:49:23 +0530

    feat:validates users

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
-PSQL="psql --username=postgres --dbname=postgres --port=5433 -t --no-align -c"
+PSQL="psql --username=postgres --dbname=number_guess --port=5433 -t --no-align -c"
 echo Enter your username:
 read USERNAME
+
+USERID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+if [[ -z $USERID ]]
+then
+    echo Welcome, $USERNAME! It looks like this is your first time here.
+else
+     echo Welcome back $USERNAME! with id $USERID
+fi
