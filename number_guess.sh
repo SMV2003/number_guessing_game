02e342188c5d5edb28b1bd7ce2e44bfcf72c778b
Shantanu Vedak <vedaksv@protonmail.com>
2024-07-24 15:39:19 +0530

    feat:added game logic

--- number_guess.sh
+++ number_guess.sh
@@ -14,3 +14,45 @@ else
      MIN_GUESSES="$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USERID")"
      echo Welcome back, $USERNAME! You have played $GAMES games, and your best game took $MIN_GUESSES guesses. 
 fi
+RNDNUM=$(( $RANDOM%1000+1 ))
+GUESS=0
+PGUESS=0
+echo Guess the number between 1-1000:$RNDNUM
+read GUESS
+if [[ ! $GUESS =~ ^[0-9]+$ ]] 
+        then
+            echo That is not a integer guess again:
+            read $GUESS
+            PGUESS=$(($PGUESS + 1))
+        fi
+PGUESS=$(($PGUESS + 1))
+while [[ $GUESS -ne $RNDNUM ]]
+do
+    if [[ $GUESS -gt $RNDNUM ]]
+    then
+        echo "It's lower than that, guess again:"
+        PGUESS=$(($PGUESS + 1))
+        read GUESS
+        if [[ ! $GUESS =~ ^[0-9]+$ ]] 
+        then
+            echo That is not a integer guess again:
+            read $GUESS
+            PGUESS=$(($PGUESS + 1))
+        fi
+    elif [[ $GUESS -lt $RNDNUM ]]
+    then
+        echo "It's higher than that, guess again:"
+        PGUESS=$(($PGUESS + 1))
+        read GUESS
+        if [[ ! $GUESS =~ ^[0-9]+$ ]] 
+        then
+            echo That is not a integer guess again:
+            read $GUESS
+            PGUESS=$(($PGUESS + 1))
+        fi
+    else
+    echo correct!
+    fi 
+done
+
+echo You guessed it in $PGUESS tries. The secret number was $RNDNUM.
