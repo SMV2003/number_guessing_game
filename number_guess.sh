4efcaf2dee2591ad4199101dbad040e895633c35
Shantanu Vedak <vedaksv@protonmail.com>
2024-07-24 13:10:22 +0530

    feat:find prev games

--- number_guess.sh
+++ number_guess.sh
@@ -8,5 +8,7 @@ if [[ -z $USERID ]]
 then
     echo Welcome, $USERNAME! It looks like this is your first time here.
 else
-     echo Welcome back $USERNAME! with id $USERID
+     GAMES="$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id=$USERID")"
+     MIN_GUESSES="$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USERID")"
+     echo Welcome back, $USERNAME! You have played $GAMES games, and your best game took $MIN_GUESSES guesses. 
 fi
